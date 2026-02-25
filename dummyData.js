8700649747b95673c8ed70ff783a7568619fbdd5
thamburutejasree0604-jpg <thamburutejasree0604@gmail.com>
2026-02-26 00:35:36 +0530

    Add files via upload

---
+++ dummyData.js
@@ -0,0 +1,233 @@
+[
+  {
+    cover:
+      "https://m.media-amazon.com/images/I/61aMBo+SFDL._AC_UL640_FMwebp_QL65_.jpg",
+    title: "A Midlife Holiday",
+    author: "Cary J Hansson",
+    price: 10.99,
+    description: "A wonderful book about a midlife holiday.",
+    details:
+      "This book is about a midlife holiday and the adventures that come with it. It is a touching and humorous story.",
+    genre: 1,
+    slug: "a-midlife-holiday",
+    qty: 10,
+  },
+  {
+    cover:
+      "https://m.media-amazon.com/images/I/813aV273-rL._AC_UL640_FMwebp_QL65_.jpg",
+    title: "It Ends with Us",
+    author: "Colleen Hoover",
+    price: 12.99,
+    description: "A heart-wrenching story of love and survival.",
+    details:
+      "This novel takes you on an emotional journey through the complexities of love and the resilience of the human spirit.",
+    genre: 3,
+    slug: "it-ends-with-us",
+    qty: 10,
+  },
+  {
+    cover:
+      "https://m.media-amazon.com/images/I/81ph7PU+AbL._AC_UL640_FMwebp_QL65_.jpg",
+    title: "The Mothers",
+    author: "Genevieve Gannon",
+    price: 15.99,
+    description: "A compelling story about motherhood and family secrets.",
+    details:
+      "This book explores the impact of a mother's choices on her family and the lengths one will go to protect their loved ones.",
+    genre: 1,
+    slug: "the-mothers",
+    qty: 10,
+  },
+  {
+    cover:
+      "https://m.media-amazon.com/images/I/81OkWjcf4WL._AC_UL640_FMwebp_QL65_.jpg",
+    title: "The Nightingale",
+    author: "Kristin Hannah",
+    price: 17.99,
+    description:
+      "A powerful story of survival and resilience during World War II.",
+    details:
+      "Follow the lives of two sisters in Nazi-occupied France and their fight for survival and freedom.",
+    genre: 6,
+    slug: "the-nightingale",
+    qty: 10,
+  },
+  {
+    cover:
+      "https://m.media-amazon.com/images/I/71hycerGPHL._AC_UL640_FMwebp_QL65_.jpg",
+    title: "The Survivors",
+    author: "Jane Harper",
+    price: 18.99,
+    description: "A gripping mystery set in a small coastal town.",
+    details:
+      "Unravel the secrets and lies that bind the residents of Evelyn Bay and the impact of past events on the present.",
+    genre: 2,
+    slug: "the-survivors",
+    qty: 10,
+  },
+  {
+    cover:
+      "https://m.media-amazon.com/images/I/917iVfhEhxL._AC_UL640_FMwebp_QL65_.jpg",
+    title: "Verity",
+    author: "Colleen Hoover",
+    price: 20.0,
+    description:
+      "A psychological thriller that keeps you on the edge of your seat.",
+    details:
+      "Discover the chilling secrets of a bestselling author as her ghostwriter unravels the truth about her life and work.",
+    genre: 7,
+    slug: "verity",
+    qty: 10,
+  },
+  {
+    cover:
+      "https://m.media-amazon.com/images/I/91TM7gPW0uL._AC_UL640_FMwebp_QL65_.jpg",
+    title: "The Four Winds",
+    author: "Kristin Hannah",
+    price: 18.99,
+    description:
+      "An epic novel of love, heroism, and hope, set against the backdrop of the Great Depression.",
+    details:
+      "Follow the journey of a woman who must fight to keep her family alive during one of the darkest periods in American history.",
+    genre: 6,
+    slug: "the-four-winds",
+    qty: 10,
+  },
+  {
+    cover:
+      "https://m.media-amazon.com/images/I/81VP99qV0yL._AC_UL640_QL65_.jpg",
+    title: "The First Harm",
+    author: "Kristin Hannah",
+    price: 8.99,
+    description:
+      "A novel about the first signs of trouble and the courage to overcome them.",
+    details:
+      "This book tells the story of how small actions can lead to significant changes and the importance of perseverance.",
+    genre: 1,
+    slug: "the-first-harm",
+    qty: 10,
+  },
+  {
+    cover:
+      "https://m.media-amazon.com/images/I/91gN2G7CEjL._AC_UL640_QL65_.jpg",
+    title: "Meet Me at the Lake",
+    author: "Carley Fortune",
+    price: 16.99,
+    description: "A romantic tale of love and second chances.",
+    details:
+      "Follow the journey of two people who reconnect at a lake, rekindling old flames and discovering new possibilities.",
+    genre: 3,
+    slug: "meet-me-at-the-lake",
+    qty: 10,
+  },
+  {
+    cover:
+      "https://m.media-amazon.com/images/I/81qgQ2eFh3L._AC_UL640_QL65_.jpg",
+    title: "Binding 13",
+    author: "Chloe Walsh",
+    price: 22.99,
+    description:
+      "A captivating story of young love and the challenges that come with it.",
+    details:
+      "This book delves into the complexities of teenage relationships and the power of love to overcome obstacles.",
+    genre: 1,
+    slug: "binding-13",
+    qty: 10,
+  },
+  {
+    cover:
+      "https://m.media-amazon.com/images/I/91BbLCJOruL._AC_UL640_QL65_.jpg",
+    title: "The Silent Patient",
+    author: "Alex Michaelides",
+    price: 20.99,
+    description:
+      "A thrilling psychological mystery that will keep you guessing until the end.",
+    details:
+      "Uncover the truth behind a woman's silence and the shocking events that led to it.",
+    genre: 7,
+    slug: "the-silent-patient",
+    qty: 10,
+  },
+  {
+    cover:
+      "https://m.media-amazon.com/images/I/71a11DtZF1L._AC_UL640_QL65_.jpg",
+    title: "How to Age Disgracefully",
+    author: "Clare Pooley",
+    price: 18.99,
+    description:
+      "A humorous and heartfelt guide to aging with grace and confidence.",
+    details:
+      "Learn how to embrace the aging process and live life to the fullest with wit and wisdom.",
+    genre: 8,
+    slug: "how-to-age-disgracefully",
+    qty: 10,
+  },
+  {
+    cover:
+      "https://m.media-amazon.com/images/I/81m973Wyr6L._AC_UL640_QL65_.jpg",
+    title: "The Midnight Feast",
+    author: "Lucy Foley",
+    price: 19.99,
+    description: "A suspenseful tale of a dinner party gone wrong.",
+    details:
+      "Follow the twists and turns of a night filled with secrets, betrayal, and unexpected revelations.",
+    genre: 2,
+    slug: "the-midnight-feast",
+    qty: 10,
+  },
+  {
+    cover:
+      "https://m.media-amazon.com/images/I/81gYjHx8YmL._AC_UL640_QL65_.jpg",
+    title: "Project Hail Mary",
+    author: "Andy Weir",
+    price: 26.79,
+    description:
+      "A science fiction adventure that takes you on an interstellar journey.",
+    details:
+      "Join a lone astronaut on a mission to save Earth from an impending disaster, facing challenges and discoveries along the way.",
+    genre: 4,
+    slug: "project-hail-mary",
+    qty: 10,
+  },
+  {
+    cover:
+      "https://m.media-amazon.com/images/I/81bO+Qz556L._AC_UL640_FMwebp_QL65_.jpg",
+    title: "The Spectacular Beneath",
+    author: "C. Erin Anderson",
+    price: 12.79,
+    description:
+      "A fantasy novel that explores the hidden wonders of an underwater world.",
+    details:
+      "Dive into an enchanting tale of adventure, magic, and the quest for truth beneath the waves.",
+    genre: 5,
+    slug: "the-spectacular-beneath",
+    qty: 10,
+  },
+  {
+    cover:
+      "https://m.media-amazon.com/images/I/61nCS552TDL._AC_UL640_QL65_.jpg",
+    title: "The Orphanage By The Lake",
+    author: "Daniel G. Miller",
+    price: 26.79,
+    description: "A historical fiction novel set during World War II.",
+    details:
+      "Discover the lives of children and caretakers at an orphanage by the lake, and the resilience they show in the face of adversity.",
+    genre: 6,
+    slug: "the-orphanage-by-the-lake",
+    qty: 10,
+  },
+  {
+    cover:
+      "https://m.media-amazon.com/images/I/81DCCd9KYSL._AC_UL640_QL65_.jpg",
+    title: "The Great Alone",
+    author: "Kristin Hannah",
+    price: 24.79,
+    description:
+      "A gripping novel of survival and resilience in the Alaskan wilderness.",
+    details:
+      "Follow the story of a family seeking a new beginning in the untamed wilderness, facing challenges and finding strength in each other.",
+    genre: 6,
+    slug: "the-great-alone",
+    qty: 10,
+  },
+];
